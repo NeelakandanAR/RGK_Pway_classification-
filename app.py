19abb294d47b9c7a5b0d3a490c75a6a337107d99
neels2708 <neelakandanbt@gmail.com>
2022-09-15 15:27:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def desc_calc():
 # File download
 def filedownload(df):
     csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
+    b64 = base58.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base58,{b58}" download="prediction.csv">Download Predictions</a>'
     return href
 
 #Decompress a Picle file
